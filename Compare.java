af427f9fd548457e9da455ddf44f802135b12930
Tanupreet <tanupreet_singh@yahoo.com>
2017-07-05 14:59:15 +0530

    compare

---
+++ Compare.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+public class Compare {
+	public static void main(String args[])
+	{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter first string");
+	String k=sc.nextLine();
+	System.out.println("Enter second string");
+	String l=sc.nextLine();
+			System.out.println(k.equals(l));
+}
+}
